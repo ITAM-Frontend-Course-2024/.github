26e5ee11758408667f93dc44e17ed29e150da228
serdeshnow <vserdes1977@mail.ru>
2024-09-14 21:58:56 +0300

    upd: ugh...

---
+++ profile/index.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 5; i++) {
+	if (i == 2) continue;
+	console.log(i);
+}
+
+/*
+  Выведет:
+  0
+  1
+  3
+  4
+*/
---
+++ profile/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  let isLoggedIn = true;
+</script>
+
+
+{#if isLoggedIn} 
+  <p>Welcome, user!</p>
+{:else}
+  <p>Please log in.</p>
+{/if}
